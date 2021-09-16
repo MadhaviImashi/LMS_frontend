8c16366ac802178b1f019d0cf2350f8fa78c3be9
Miru <madhaviimashi@gmail.com>
2021-09-16 07:58:39 +0530

    testing styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+//import the styled-components
+import styled from "styled-components/";
+
+//create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  pading: 4em;
+  background: papayawhip;
+`;
 
 function App() {
   return (
-    <h1 style={{ textAlign: "center" }}>
-      Hi, Welcome
-    </h1>
+    <Wrapper>
+      <Title> Hi, Welcome</Title>
+    </Wrapper>
   );
 }
 
