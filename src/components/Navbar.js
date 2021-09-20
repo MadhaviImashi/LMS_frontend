eb699d5281ea6b6cf2a23b196115d6061ef6a018
Miru <madhaviimashi@gmail.com>
2021-09-20 22:33:41 +0530

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,18 @@ import styled from 'styled-components';
 export const NavBar = styled.ul`
     list-style: none;
     margin: 0;
-    padding: 0;
+    padding: 1em 0;
     overflow: hidden;
     background-color: ${(props) => props.theme.primary.main};
-    position: fixed;
-    top: 0;
     width: 100%;
-    z-index: 1;
 `;
 //instead of hard coding the colors everywhere, now u can use those defined colors in our theme obj in the App.js class using 'theme' prop
 //we can embed javascript codes using ${} inside these Template literals which encloses with backticks
 export const NavItem = styled.li`
-    float: left;
+    display: inline;
 `;
 
 export const NavLink = styled.a`
-    display: block;
     color: ${(props) => props.theme.textColor};
     text-align: center;
     padding: 14px 16px;
---
+++ src/components/Tabs.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Tabs = styled.div`
+    overflow: hidden;
+    background: #fff;
+    height: 100%;
+`;
+//overflow: hidden - to show the content of the selected tab only(rest of the content will be hidden)
+
+export const Tab = styled.button`
+    border: none;
+    outline: none;
+    cursor: pointer;
+    possition: relative;
+    margin-right: 0.1em;
+    font-size: 1em;
+    border: ${(props) => (props.active ? "" : "1px solid #ccc")};
+    border-bottom: none;
+    background-color: ${(props) => (props.active ? "#fff": "#000")};
+    border-top-left-radius: 1em;
+    border-top-right-radius: 1em;
+    :hover{
+        background-color: #4fc3f7;
+    }
+`;
+
+export const TabContent = styled.div`
+    border: 0.25em solid #039be5;
+    border-top: 0.5em solid #039be5;
+    border-top-right-radius: 1em;
+    border-bottom-left-radius: 1em;
+    border-bottom-right-radius: 1em;
+    min-height: 80vh;
+`;
+
+export const Content = styled.div`
+    #{(props) => (props.active ? "" : "display: none")}
+`;
+//display: none will not display anything if the component state is not active*
