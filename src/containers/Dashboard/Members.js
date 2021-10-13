d91ab7d79d6d69572739f23a3fb5c482c61300d3
Miru <madhaviimashi@gmail.com>
2021-10-13 10:11:08 +0530

    checked wether member is populating data in the table

--- src/containers/Dashboard/Members.js
+++ src/containers/Dashboard/Members.js
@@ -1,22 +1,23 @@
 import React from "react";
-import Table from "../../components/Table";
+//import Table from "../../components/Table";
 import { FluidContainer } from "../../components/CommonComponents";
 
 
 //pass a prop called bookCatalog to this function
 export const Members = ({membersCatalog}) => {
 
-    const handleTableRowClick = (id) => {
-        console.log(id);
-    }
+    // const handleTableRowClick = (id) => {
+    //     console.log(id);
+    // }
     
     return (
         <FluidContainer>
-            {/* {membersCatalog[1] && membersCatalog[1].Name 
-                ? membersCatalog[1].Name
-                : "Still loading"} */}
 
-            <Table data={membersCatalog} handleRowClick={handleTableRowClick} instruction="Click row to view member"/>
+            {membersCatalog[1] && membersCatalog[1].Name 
+                ? membersCatalog[1].Name
+                : "Still loading"}
+
+            {/* <Table data={membersCatalog} handleRowClick={handleTableRowClick} instruction="Click row to view member"/> */}
 
         </FluidContainer>
     );
