959292f615ed5e442de6ed952b82356d2873597a
Miru <madhaviimashi@gmail.com>
2021-10-18 13:48:16 +0530

    all the pi calls created

--- src/api/memberAPI.js
+++ src/api/memberAPI.js
@@ -2,9 +2,13 @@
 //thses api calls will be used to communicate with our Backend
 
 //each api call(ex:getBooks) use a relevant http request function(ex:getRequest) to communicate with the backend.Therefor, import those request functions
-import { getRequest } from "./util";
+import { deleteRequest, getRequest, postRequest, putRequest } from "./util";
 
 const BASE_URL = "/member";
 
 export const getMembers = () => getRequest(`${BASE_URL}`);
 export const getMember = (id) => getRequest(`${BASE_URL}/${id}`);
+
+export const addMember = (data) => postRequest(`${BASE_URL}`, data);
+export const deleteMember = (id) => deleteRequest(`${BASE_URL}/${id}`);
+export const updateMember = (id, data) => putRequest(`${BASE_URL}/${id}`, data);
