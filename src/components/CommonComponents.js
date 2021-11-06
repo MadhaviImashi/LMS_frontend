818338cea4db234b555270b1a361945ac3f8f8a1
MadhaviImashi <madhaviimashi@gmail.com>
2021-11-07 00:57:05 +0530

    comment changed

--- src/components/CommonComponents.js
+++ src/components/CommonComponents.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"; //import styled-components
+import styled from "styled-components"; //styled-components
 
 export const FluidContainer = styled.div`
     display: flex;
